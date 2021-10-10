5218e1f05e5eb9b22c603b8107ab21a92a441948
Aman Verma <amanav529@gmail.com>
2021-10-10 21:35:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -55,6 +55,10 @@ let link= document.getElementById("que");
 let time = new Date();
 if(time.getHours()>7 && time.getHours()<19)
   link.removeAttribute("href");
+else if(time.getHours()>=0 && time.getHours()<7)
+	link.setAttribute("href",questions[time.getDate()-12])
 else
-	link.setAttribute("href",questions[time.getDate()-11])
+    link.setAttribute("href",questions[time.getDate()-11])
+
+
 
